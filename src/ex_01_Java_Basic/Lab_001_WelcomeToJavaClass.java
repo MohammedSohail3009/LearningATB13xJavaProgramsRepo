defe106178face296d7e6d63da0d8d120e745286
Mohammedsohail3009 <sohail2111@gmail.com>
2025-06-15 12:01:36 -0700

    Welcome to Java updated

--- src/ex_01_Java_Basic/Lab_001_WelcomeToJavaClass.java
+++ src/ex_01_Java_Basic/Lab_001_WelcomeToJavaClass.java
@@ -1,4 +1,6 @@
-public class hello {
+package ex_01_Java_Basic;
+
+public class Lab_001_WelcomeToJavaClass {
     public static void main(String[] args){
         System.out.println("Welcome to java");
         System.out.println("Hello world");
