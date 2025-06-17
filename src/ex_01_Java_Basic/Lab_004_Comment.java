e06826472ad9a65383d4976163cf0128e3948594
Mohammedsohail3009 <sohail2111@gmail.com>
2025-06-17 11:54:25 -0700

    Welcome to Java updated

---
+++ src/ex_01_Java_Basic/Lab_004_Comment.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab_004_Comment {
+    public static void main(String[] args) {
+        // This is a commented code
+        System.out.println("Hi");
+        System.out.println("Byee");
+   }
+}
---
+++ src/ex_01_Java_Basic/Lab_005_keywords_Identifiers.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab_005_keywords_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("KeyWords and Identifiers");
+        //package public class static void - Identifiers
+    }
+}
