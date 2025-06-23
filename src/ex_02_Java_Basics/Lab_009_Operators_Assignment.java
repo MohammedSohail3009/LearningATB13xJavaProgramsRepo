8238626857d2c047b959af0eae2acda0c82dbd9e
Mohammedsohail3009 <sohail2111@gmail.com>
2025-06-23 11:44:19 -0700

    Welcome to Java updated

---
+++ src/ex_02_Java_Basics/Lab_009_Operators_Assignment.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab_009_Operators_Assignment {
+    public static void main(String[] args){
+        int age = 65;
+        System.out.println(age);
+
+
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab_10_Strings_Immutable.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics;
+
+public class Lab_10_Strings_Immutable {
+    public static void main(String[] args) {
+        String name = "Sufi";
+        name =name.toUpperCase();
+        System.out.println(name);
+    }
+}
