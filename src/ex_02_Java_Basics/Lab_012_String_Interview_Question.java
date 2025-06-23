41cb9a2a14e06a2a3074e4a9f70188e90d763b84
Mohammedsohail3009 <sohail2111@gmail.com>
2025-06-23 12:03:38 -0700

    Welcome to Java updated

--- src/ex_02_Java_Basics/Lab_012_String_Interview_Question.java
+++ src/ex_02_Java_Basics/Lab_012_String_Interview_Question.java
@@ -3,8 +3,10 @@ package ex_02_Java_Basics;
 public class Lab_012_String_Interview_Question {
     public static void main(String[] args) {
         String s1 = "ABCD";
+        System.out.println(s1);
         System.out.println(s1.length());
         System.out.println(s1.toLowerCase());
         System.out.println(s1.toUpperCase());
+        System.out.println(s1.concat("E"));
     }
 }
