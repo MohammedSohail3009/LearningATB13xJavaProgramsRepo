d43d012d5ea68f8dac461831f35ca18a916667cb
Mohammedsohail3009 <sohail2111@gmail.com>
2025-06-23 11:55:16 -0700

    Welcome to Java updated

---
+++ src/ex_02_Java_Basics/Lab_11_Strings_Concat.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics;
+
+public class Lab_11_Strings_Concat {
+    public static void main(String[] args) {
+        String s1 = "Sufi";
+       s1= s1.concat("Maldar");
+        System.out.println(s1);
+    }
+}
