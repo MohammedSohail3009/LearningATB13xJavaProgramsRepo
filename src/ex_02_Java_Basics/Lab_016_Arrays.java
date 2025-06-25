937d870c4a94bf906b4f6aaa6926c2e970f593fc
Mohammedsohail3009 <sohail2111@gmail.com>
2025-06-25 11:59:23 -0700

    Welcome to Java updated

---
+++ src/ex_02_Java_Basics/Lab_016_Arrays.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab_016_Arrays {
+    public static void main(String[] args) {
+       int[] marks = {67,98,76,89,56,76,88};
+        System.out.println(marks.length);
+        System.out.println(marks[0]);
+        System.out.println(marks[4]);
+    }
+}
---
+++ src/ex_02_Java_Basics/Lab_017_Arrays_2.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+
+public class Lab_017_Arrays_2 {
+    public static void main(String[] args) {
+//        int[] marks = new int[5];
+        String[] names = new String[4];
+        names[0] = "Mohammed";
+        names[1] = "Sufiyan";
+        names[2] = "Maldar";
+        names[3] = "Sohail";
+        System.out.println(names[3]);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+    }
+}
