f204a310df16ee5434c9de33fb1e5746ff1e6d9a
Mohammedsohail3009 <sohail2111@gmail.com>
2025-06-29 01:33:37 -0700

    Welcome to Java updated

---
+++ src/ex_02_Java_Basics/Lab_020_Relational_Operators.java
@@ -0,0 +1,25 @@
+package ex_02_Java_Basics;
+
+public class Lab_020_Relational_Operators {
+    // < less than
+    // > Greater than
+    // <= less than or equal to
+    // >= Greater than or equal to
+    // == Equal to(but checking)
+    // != -> Not Equal
+    public static void main(String[] args) {
+
+
+
+    int a = 20;
+    int b = 35;
+    boolean c = a > b;
+System.out.println(c);
+
+int age_maldar = 40;
+int age_sufi = 45;
+boolean result = age_maldar<=age_sufi;
+System.out.println(result);
+
+}
+}
