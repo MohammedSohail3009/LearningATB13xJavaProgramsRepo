97b5ed96cbb1c15c6e1e0148fed9df01ed889c48
Mohammedsohail3009 <sohail2111@gmail.com>
2025-06-29 02:08:02 -0700

    Welcome to Java updated

---
+++ src/ex_02_Java_Basics/Lab_022_Interview_Concatplus.java
@@ -0,0 +1,15 @@
+package ex_02_Java_Basics;
+
+public class Lab_022_Interview_Concatplus {
+    public static void main(String[] args) {
+        String first_name = "Sufiyan";
+        String last_name = "Maldar";
+        int a = 10;
+        int b = 10;
+        System.out.println(first_name+last_name+a+b);
+        System.out.println(a+b+first_name+last_name);
+        System.out.println(first_name+last_name+(a+b)); //Bodmas principle
+
+
+    }
+}
