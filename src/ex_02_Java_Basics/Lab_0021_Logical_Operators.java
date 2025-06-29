8b6ce3255350d4f82b214763ab4575cab806ad66
Mohammedsohail3009 <sohail2111@gmail.com>
2025-06-29 01:51:41 -0700

    Welcome to Java updated

---
+++ src/ex_02_Java_Basics/Lab_0021_Logical_Operators.java
@@ -0,0 +1,16 @@
+package ex_02_Java_Basics;
+
+public class Lab_0021_Logical_Operators {
+    public static void main(String[] args) {
+        boolean a = true;
+        System.out.println(!a);
+
+        boolean b = true;
+        System.out.println(!!b);
+
+        boolean c = true || false;
+        System.out.println(c);
+        boolean c1= true && false;
+        System.out.println(c1);
+    }
+}
