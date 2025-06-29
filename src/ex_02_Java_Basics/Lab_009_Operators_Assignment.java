b5ac00e01bcdeb754dba172fdd15945cb4e3ce14
Mohammedsohail3009 <sohail2111@gmail.com>
2025-06-29 00:00:24 -0700

    Welcome to Java updated

--- src/ex_02_Java_Basics/Lab_009_Operators_Assignment.java
+++ src/ex_02_Java_Basics/Lab_009_Operators_Assignment.java
@@ -4,12 +4,14 @@ public class Lab_009_Operators_Assignment {
     public static void main(String[] args){
        // int age = 65;
         //System.out.println(age);
-        int a = 25;
-        int b = 15;
+        int a = 20;
+        int b = 3;
+        float c = 3.0f;
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a/b);
         System.out.println(a%b);
+        System.out.println(a/c);
 
 
     }
