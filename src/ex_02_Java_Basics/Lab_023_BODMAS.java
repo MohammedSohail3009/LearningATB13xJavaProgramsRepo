0cf24a810e381934e812d9baaff4a283689ed844
Mohammedsohail3009 <sohail2111@gmail.com>
2025-06-29 02:13:56 -0700

    Welcome to Java updated

---
+++ src/ex_02_Java_Basics/Lab_023_BODMAS.java
@@ -0,0 +1,11 @@
+package ex_02_Java_Basics;
+
+public class Lab_023_BODMAS {
+    public static void main(String[] args) {
+        System.out.println((8*3/8+1)*3);
+        //8*3 = 27
+        // 27/8= 3
+        //3+1 = 4
+        //4*3 = 12
+    }
+}
