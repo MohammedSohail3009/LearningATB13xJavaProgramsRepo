0a1466cd00d02d0cd3472672e59a24935b7be84f
Mohammedsohail3009 <sohail2111@gmail.com>
2025-09-11 06:30:41 -0700

    Welcome to Java updated

---
+++ src/ex_02_Java_Basics/Lab_019_Modulus_Operators.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab_019_Modulus_Operators {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        System.out.println(a%b);
+        System.out.println(13%7);
+    }
+}
