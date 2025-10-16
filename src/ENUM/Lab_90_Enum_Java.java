cb46ae058d42cc832b278fae49f05ca3a9047a46
Mohammedsohail3009 <sohail2111@gmail.com>
2025-10-16 07:10:30 -0700

    Welcome to Java updated

---
+++ src/ENUM/Lab_90_Enum_Java.java
@@ -0,0 +1,9 @@
+package ENUM;
+
+public class Lab_90_Enum_Java {
+    public static void main(String[] args){
+        System.out.println(APIURLs.vwo.getUrl());
+        System.out.println(APIURLs.google.getUrl());
+        System.out.println(APIURLs.katalon.getUrl());
+    }
+}
---
+++ src/StaticBlock/Lab_009_WebAutomation.java
@@ -0,0 +1,16 @@
+package StaticBlock;
+
+public class Lab_009_WebAutomation {
+    public static void main(String[] args){
+        Automation t1 = new Automation();
+        System.out.println(t1.driver);
+        Automation t2 = new Automation();
+        Automation t3 = new Automation();
+        System.out.println(t2.driver);
+        System.out.println(t3.driver);
+
+    }
+}
+class Automation{
+    static String driver = "chrome";
+}
